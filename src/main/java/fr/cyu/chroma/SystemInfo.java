b5d77d67e22b732d5e3d5387155814bca50849bf
Reight123 <clement_praud@yahoo.com>
2024-05-07 13:18:17 +0200

    init: Project

---
+++ src/main/java/fr/cyu/chroma/SystemInfo.java
@@ -0,0 +1,13 @@
+package fr.cyu.chroma;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}

c1d408684c930253d1d476571bd7e9f1a81df311
gaetan <gaetan.g237@gmail.com>
2024-05-15 22:27:58 +0200

    feat: check if maven is installed before call it

--- run_linux.bash
+++ run_linux.bash
@@ -1,2 +1,18 @@
 #!/bin/bash
+
+if [ -z "$MAVEN_HOME" ]; then
+        echo "Maven isn't installed (\$MAVEN_HOME variable undefined)"
+        exit 1
+else
+    if [ ! -d "$MAVEN_HOME/bin" ]; then
+        echo "Maven installation incomplete (\$MAVEN_HOME/bin not found)"
+        exit 1
+    else
+        if [ ! -f "$MAVEN_HOME/bin/mvn" ]; then
+            echo "Maven installation incomplete (\$MAVEN_HOME/bin/mvn not found)"
+            exit 1
+        fi
+    fi
+fi
+
 mvn -f chromat_ynk/pom.xml clean javafx:run
--- run_windows.bat
+++ run_windows.bat
@@ -1,2 +1,9 @@
 @echo off
+
+if "%MAVEN_HOME%"=="" (
+    echo .
+    echo "Maven isn't installed (%MAVEN_HOME% variable undefined)"
+    exit /b 1
+)
+
 mvn -f chromat_ynk\pom.xml javafx:run

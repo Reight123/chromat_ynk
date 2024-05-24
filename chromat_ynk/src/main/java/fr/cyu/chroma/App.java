865af76639ec2dfc0653ee279b18287f9c7558f3
rabtiio <matthias.ribeiro.2003@gmail.com>
2024-05-24 09:52:10 +0200

    dev: documentation App

--- chromat_ynk/src/main/java/fr/cyu/chroma/App.java
+++ chromat_ynk/src/main/java/fr/cyu/chroma/App.java
@@ -37,13 +37,17 @@ public class App extends Application {
     private VBox messageBox = new VBox();
     private int drawingWindowWidth = 800;
     private int drawingWindowHeight = 800;
+    /**
+    this function puts everything the app needs in primaryStage
 
+     @param primaryStage is the window of the app
+     */
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Menu");
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
 
-        VBox vbox = new VBox();
+        VBox vbox = new VBox();             //creation of the box where the user put his commands
         vbox.setPadding(new Insets(40));
         vbox.setSpacing(10);
         vbox.getStylesheets().add(("/style.css"));
@@ -56,7 +60,7 @@ public class App extends Application {
 
         addButtons(vbox);
 
-        ChoiceBox<String> choiceBox = new ChoiceBox<>();
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();    //create the list wich showw the choice of command
         choiceBox.getStyleClass().add("choice");
         choiceBox.setItems(choices);
         choiceBoxes.add(choiceBox);
@@ -81,7 +85,7 @@ public class App extends Application {
         addButtons.add(addButton);
         deleteButtons.add(deleteButton);
 
-        messageBox = new VBox();
+        messageBox = new VBox();        //create the box where the errors will be show
         messageBox.getStyleClass().add("gbox");
         messageBox.setPadding(new Insets(10));
         messageBox.setSpacing(5);
@@ -91,20 +95,27 @@ public class App extends Application {
         primaryStage.show();
     }
 
-
+    /**
+     * this function will show all the new errors in the box made for
+     * @param items messages shown for the errors
+     * @param messageBox the box where the messages will be shown
+     */
     private static void updateMessageBox(ObservableList<String> items, VBox messageBox) {
         messageBox.getChildren().clear();
-        Label label = new Label("Erreur :");
+        Label label = new Label("Erreur :");    //the first line is always Erreur:
         messageBox.getChildren().add(label);
-        for (String item : items) {
+        for (String item : items) {         //show every messages line by line
             label = new Label(item);
             messageBox.getChildren().add(label);
         }
     }
 
 
-
-
+    /**
+     * add a new vlock after users press the "+" button
+     * @param vbox where we will put the new choice box
+     * @param previousChoiceBox the one where the user was when he press the "+" buttonmessage begin
+     */
     private void addNewBlockAfter(VBox vbox, ChoiceBox<String> previousChoiceBox) {
         int index = choiceBoxes.indexOf(previousChoiceBox) + 1;
         ChoiceBox<String> choiceBox = new ChoiceBox<>();
@@ -133,6 +144,11 @@ public class App extends Application {
         deleteButtons.add(deleteButton);
     }
 
+    /**
+     * delete the box where the user was when he press the "-" button
+     * @param vbox
+     * @param choiceBox the bow wiwh will be delete
+     */
     private void deleteBlock(VBox vbox, ChoiceBox<String> choiceBox) {
         if (choiceBoxes.size() > 1) {
             int index = choiceBoxes.indexOf(choiceBox);
@@ -145,6 +161,10 @@ public class App extends Application {
         }
     }
 
+    /**
+     * add the save, load and execute button
+     * @param vbox
+     */
     private void addButtons(VBox vbox) {
         Button writeButton = new Button("Enregistrer dans un fichier");
         writeButton.getStyleClass().add("button");
@@ -165,11 +185,14 @@ public class App extends Application {
             executeFile(file);
         });
 
-        HBox buttonBox = new HBox(10);
+        HBox buttonBox = new HBox(10); // add the three buttons on the screen
         buttonBox.getChildren().addAll(writeButton, selectFileButton, selectThisFileButton);
         vbox.getChildren().add(buttonBox);
     }
 
+    /**
+     * when the save button is pressed this function will make the user choose the name and then save it
+     */
     private void writeCommand() {
         try {
             Stage stage = new Stage();
@@ -178,11 +201,11 @@ public class App extends Application {
             vbox.setPadding(new Insets(10));
             vbox.setSpacing(5);
             TextField fileNameField = new TextField();
-            fileNameField.setPromptText("Entrez le nom du fichier");
+            fileNameField.setPromptText("Entrez le nom du fichier");    //the user chooses the name of the file
             Button confirmButton = new Button("Confirmer");
             confirmButton.setOnAction(event -> {
                 String fileName = fileNameField.getText();
-                if (!fileName.isEmpty()) {
+                if (!fileName.isEmpty()) { //save it only if the name is not empty
                     saveToFile(fileName);
                     stage.close();
                 }
@@ -197,25 +220,29 @@ public class App extends Application {
         }
     }
 
+    /**
+     * this function save the file on a .txt file
+     * @param fileName the name of the file
+     */
     private void saveToFile(String fileName) {
         try {
-            File file = new File("./storage/" + fileName + ".txt");
+            File file = new File("./storage/" + fileName + ".txt");  //create the .txt fill in the absolute path
             FileWriter fileWriter = new FileWriter(file);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-            for (int i = 0; i < choiceBoxes.size(); i++) {
+            for (int i = 0; i < choiceBoxes.size(); i++) {      //write in the fill all the command
                 String selectedOption = choiceBoxes.get(i).getValue();
                 String enteredValue = valueFields.get(i).getText();
                 if (selectedOption != null) {
-                    if (selectedOption.equals("FOR") || selectedOption.equals("IF") || selectedOption.equals("WHILE") || selectedOption.equals("MIMIC")|| selectedOption.equals("MIRROR")) {
+                    if (selectedOption.equals("FOR") || selectedOption.equals("IF") || selectedOption.equals("WHILE") || selectedOption.equals("MIMIC")|| selectedOption.equals("MIRROR")) {    //those command open a loop
                         bufferedWriter.write(selectedOption + " " + enteredValue + "\n");
                         bufferedWriter.write("{\n");
-                    } else if (selectedOption.equals("ENDBLOCK") || selectedOption.equals("ENDMIRROR") ) {
+                    } else if (selectedOption.equals("ENDBLOCK") || selectedOption.equals("ENDMIRROR") ) {      //they close a loop
                         bufferedWriter.write("}\n");
-                    } else if (selectedOption.equals("MIMICEND")) {
+                    } else if (selectedOption.equals("MIMICEND")) { //it also close a loop but need to be written
                         bufferedWriter.write("}\n");
                         bufferedWriter.write(selectedOption + " " + enteredValue + "\n");
-                    } else if (!enteredValue.isEmpty()) {
+                    } else if (!enteredValue.isEmpty()) {       //all the others commands
                         bufferedWriter.write(selectedOption + " " + enteredValue + "\n");
                     }
                 }
@@ -228,16 +255,24 @@ public class App extends Application {
         }
     }
 
+    /**
+     * select the file wich will be executed
+     * @return the file selected
+     */
     private File selectFile() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Sélectionner un fichier");
         String directory = "storage/";
-        File repertoireInitial = new File(directory);
+        File repertoireInitial = new File(directory);   //open the file selectionner in storage/
         fileChooser.setInitialDirectory(repertoireInitial);
-        File selectedFile = fileChooser.showOpenDialog(null);
+        File selectedFile = fileChooser.showOpenDialog(null);       //select the choosen file
         return selectedFile;
     }
 
+    /**
+     * execute the selected file
+     * @param selectedFile the file wich will be execute
+     */
     private void executeFile(File selectedFile) {
         if (selectedFile == null) {
             selectedFile = selectFile();
@@ -251,14 +286,14 @@ public class App extends Application {
         if (!fileContent.isEmpty()) {
             try {
                 Interpreter interpreter = new Interpreter(this.drawingWindowWidth, this.drawingWindowHeight);
-                javaCode = interpreter.decode(fileContent);
+                javaCode = interpreter.decode(fileContent);             //turn the selected file into a java script
                 File templateFile = new File("../plotter/src/main/template/templateMain.java");
                 String temp = getFileContent(templateFile);
-                String[] template = temp.split("//insertion area do not delete//");
+                String[] template = temp.split("//insertion area do not delete//"); //find in the executable where it can put the script
 
                 if (template.length == 2 && !temp.isEmpty()) {
                     javaCode = template[0] + javaCode + template[1];
-                    writeJavaFile(javaCode);
+                    writeJavaFile(javaCode);    //put the scipt in the executable
                     if (!javaCode.isEmpty()) {
                         Thread thread = new Thread(() -> run(this, messageBox));
                         thread.start();
@@ -275,14 +310,18 @@ public class App extends Application {
     }
 
 
-
+    /**
+     * return the content of the file
+     * @param selectedFile the file wich user want the content
+     * @return the content of the file
+     */
     private String getFileContent(File selectedFile) {
         String fileContent = "";
         if (selectedFile != null) {
             try (BufferedReader br = new BufferedReader(new FileReader(selectedFile))) {
                 StringBuilder content = new StringBuilder();
                 String line;
-                while ((line = br.readLine()) != null) {
+                while ((line = br.readLine()) != null) {    //read line by line
                     content.append(line).append("\n");
                 }
                 fileContent = content.toString();
@@ -296,6 +335,10 @@ public class App extends Application {
         }
     }
 
+    /**
+     * write the content in a .java file
+     * @param fileContent the content it will put in the file
+     */
     private void writeJavaFile(String fileContent) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("../plotter/src/main/java/fr/cyu/chroma/Main.java"))) {
             writer.write(fileContent);
@@ -304,17 +347,21 @@ public class App extends Application {
         }
     }
 
-
+    /**
+     * execute the current script when the user clicks on the run button
+     * @param appInstance
+     * @param messageBox
+     */
     private static void run(App appInstance, VBox messageBox) {
         try {
             String pathPlotter = "../plotter/pom.xml";
             String osName = System.getProperty("os.name").toLowerCase();
             ProcessBuilder processBuilder;
-            if (osName.contains("windows")) {
+            if (osName.contains("windows")) {   //Writes the process differently depending on the device's operating system.
                 processBuilder = new ProcessBuilder("cmd.exe", "/c", "mvn", "-f", pathPlotter, "clean", "javafx:run");
             } else if (osName.contains("linux") || osName.contains("mac")) {
                 processBuilder = new ProcessBuilder("mvn", "-f", pathPlotter, "clean", "javafx:run");
-            } else {
+            } else {    //error message when the os is neither windows, mac or linux
                 Platform.runLater(() -> {
                     ObservableList<String> unsupportedOsMessage = observableArrayList("OS not supported.");
                     updateMessageBox(unsupportedOsMessage, messageBox);
